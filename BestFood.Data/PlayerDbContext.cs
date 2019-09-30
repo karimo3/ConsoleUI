9ec28ad22e23bec4b57ecfde2f0e691a35946b76
karimo3 <kokarim3@outlook.com>
2019-09-30 18:34:31 -0400

    removed players pages

---
+++ BestFood.Data/PlayerDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurants.Data
+{
+    public class PlayerDbContext : DbContext
+    {
+        public PlayerDbContext(DbContextOptions<PlayerDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Player> Restaurants { get; set; }
+    }
+}
